2c5f63020cff9dcfa404afcb55f208b1adec5ea1
Vinayak More <vinayak.more@infibeam.net>
2026-04-04 13:44:27 +0530

    Add: burst-balloons - submission-5

---
+++ Data Structures & Algorithms/burst-balloons/submission-5.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] newNums = new int[n + 2];
+        newNums[0] = newNums[n + 1] = 1;
+        for (int i = 0; i < n; i++) {
+            newNums[i + 1] = nums[i];
+        }
+
+        int[][] dp = new int[n + 2][n + 2];
+        for (int l = n; l >= 1; l--) {
+            for (int r = l; r <= n; r++) {
+                for (int i = l; i <= r; i++) {
+                    int coins = newNums[l - 1] * newNums[i] * newNums[r + 1];
+                    coins += dp[l][i - 1] + dp[i + 1][r];
+                    dp[l][r] = Math.max(dp[l][r], coins);
+                }
+            }
+        }
+
+        return dp[1][n];
+    }
+}
