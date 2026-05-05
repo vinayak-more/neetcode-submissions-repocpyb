5491212fe29d8bc2746c0c22a7191d45cbc93daa
Vinayak More <vinayak.more@infibeam.net>
2026-05-05 13:47:13 +0530

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int mask = 0;
+        mask ^= nums.length;
+        for(int i = 0; i < nums.length; i++){
+            mask ^= i ^ nums[i];
+        }
+        return mask;
+    }
+}
